6e5635c7f0006e9b8929e2da2ea8017a4afe48ce
root <vfulber@inf.ufsm.br>
2018-11-13 23:18:17 -0200

    Load Balancer Update

--- Python/LoadBalancer.py
+++ Python/LoadBalancer.py
@@ -2,27 +2,30 @@ import socket
 import threading
 from scapy.all import *
 
-standardIface = 'eth0'
+firstIface = 'eth0'
 firstIfaceFlows = ['52:54:00:42:84:65']
-serverPaths = ['eth1', 'eth2']
-serverPathsFlows = [['52:54:00:a1:54:c0'], ['52:54:00:a1:54:c0']]
+secondIface = 'eth1'
+secondIfaceFlows = ['52:54:00:a1:54:c0']
+thirdIface = 'eth2'
+thirdIfaceFlows = ['52:54:00:d3:a3:df']
 
 def inOutServer():
 
-    global standardIface
-    global serverPaths
+    global firstIface
+    global secondIface
+    global thirdIface
+    global firstIfaceFlows
 
     inSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
-    inSocket.bind((standardIface, 0))
+    inSocket.bind((firstIface, 0))
 
-    serverSockets = []
-    serverQuantity = len(serverPaths)
+    outSocket01 = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+    outSocket01.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1514)
+    outSocket01.bind((secondIface, 0))
 
-    for path in serverPaths:
-    	sSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
-    	sSocket.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1514)
-    	sSocket.bind((path, 0))
-    	serverSockets.append(sSocket)
+    outSocket02 = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+    outSocket02.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1514)
+    outSocket02.bind((thirdIface, 0))
 
     while True:
 
@@ -30,44 +33,42 @@ def inOutServer():
         try:
             et = Ether(bytes(pkt[0]))
             if not et.src in firstIfaceFlows:
-                continue
+                continue 
+            print('eth0 -> ' + str(et.src))
         except:
             continue
 
-        if IP in et:
-        	serverSockets[int((et[IP].src).split('.')[-1])%serverQuantity].send(bytes(et))
-        else:
-        	serverSockets[0].send(bytes(et))
+        outSocket01.send(bytes(et))        
 
-def outInServer(inIface, inIfaceFlows):
+def outInServer(iface, ifaceFlows):
 
-    global standardIface
+    global firstIface
 
-    inSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
-    inSocket.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1514)
-    inSocket.bind((inIface, 0))
+    inSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
+    inSocket.bind((iface, 0))
 
-    outSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
-    outSocket.bind((standardIface, 0))
+    outSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+    outSocket.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1514)
+    outSocket.bind((firstIface, 0))
 
     while True:
         
         pkt = inSocket.recvfrom(1514)
         try:
             et = Ether(bytes(pkt[0]))
-            if not et.src in inIfaceFlows:
+            if not et.src in ifaceFlows:
                 continue
         except:
             continue
-
+        
         outSocket.send(bytes(et))
 
-
-outInSockets = []
 inOut = threading.Thread(target=inOutServer,args=())
-for index in range(len(serverPaths)):
-    outIn = threading.Thread(target=outInServer,args=(serverPaths[index], serverPathsFlows[index]))
-    outIn.start()
-    outInSockets.append(outIn)
+outIn01 = threading.Thread(target=outInServer,args=(secondIface, secondIfaceFlows))
+outIn02 = threading.Thread(target=outInServer,args=(thirdIface, thirdIfaceFlows))
+outIn02.start()
+outIn01.start()
 inOut.start()
 inOut.join()
+outIn01.join()
+outIn02.join()
